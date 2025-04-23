537dc92ec880dc2afd051ccff1ed07d854ea4273
mbien1981 <shieldhater@gmail.com>
2025-04-23 19:17:54 +0200

    Add mask_selector v2.1 w/source

---
+++ src/mask_selector/base.lua
@@ -0,0 +1,16 @@
+return DMod:new("custom_mask_selector", {
+	name = "Custom Mask Selector Interface",
+	author = "_atom",
+	version = "2.1",
+	categories = { "qol" },
+	dependencies = { "[drop_in_menu]", "[character_n_mask_in_loadout]" },
+	hooks = {
+		{ "post_require", "lib/setups/setup", "mask_selector" },
+		{ "post_require", "lib/states/ingamewaitingforplayers", "mask_selector" },
+		{ "post_require", "lib/managers/menumanager", "mask_selector" },
+	},
+	update = {
+		id = "mask_selector",
+		url = "https://raw.githubusercontent.com/mbien1981/dahm-modules/main/version.json",
+	},
+})
